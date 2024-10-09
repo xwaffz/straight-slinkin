c1ea72453f764e54fa15b04910c7e7e554b494cb
Anthony-Lagreca <94651568+Anthony-Lagreca@users.noreply.github.com>
2024-10-08 22:42:14 -0400

    fixed coyote time and updated movement!!

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,19 +12,20 @@ var coyote_frames = 6
 @onready var kill_zone: Area2D = $"../KillZone"
 @onready var collision_shape_2d: CollisionShape2D = $"../KillZone/CollisionShape2D"
 @onready var coyote_timer: Timer = $CoyoteTimer
+var last_direction = 0  # Track the last direction
 
 
 func _on_coyote_timer_timeout():
 	coyote = false
+	print("helloo")
 
 func _ready() -> void:
-	$CoyoteTimer.wait_time = coyote_frames / 60.0
+	coyote_timer.wait_time = 0.15
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta:float) -> void:
 	if Input.is_action_just_pressed("restart"):
 		Engine.time_scale = 1
 		get_tree().reload_current_scene()
-		
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -33,36 +34,36 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote):
 		velocity.y = JUMP_VELOCITY
-		var jumping = true
 		coyote = false
-
-	# Get the input direction: -1, 0, 1
-	var direction := Input.get_axis("move_left", "move_right")
-	
-	# Flip the Sprite
-	if direction > 0:
-			animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
 		
+
+	# Check input direction: -1 for left, 0 for none, 1 for right
+	if Input.is_action_pressed("move_right"):
+		last_direction = 1
+	elif Input.is_action_pressed("move_left"):
+		last_direction = -1
+	else: 
+		last_direction = 0
+
+	# Set velocity based on the last direction
+	velocity.x = last_direction * SPEED
+
+	# Flip the Sprite
+	animated_sprite.flip_h = last_direction < 0
+
 	var was_on_floor = is_on_floor()
 	move_and_slide()
-	
-	if !is_on_floor() and was_on_floor and velocity.y>=0:
+
+	if !is_on_floor() and was_on_floor and velocity.y >= 0:
 		coyote = true
-		$CoyoteTimer.start()
-		
+		coyote_timer.start()
+		print("hi")
+
 	# Play animations
 	if is_on_floor():
-		if direction == 0:
+		if last_direction == 0:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-	
-	# Apply movement
-	if direction :
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
